ef8c4c16088fa8aa0ed32d44df962b7b4ae7df16
jsiriw01 <dananjaya@live.com>
2016-08-26 12:11:52 +0530

    Add files via upload

---
+++ AppProperties.java
@@ -0,0 +1,42 @@
+/*
+ 
+ * Author: jayatunga Siriwardana
+ * Date: 21/8/2016
+ * Version: 
+ * Moderator:
+ * Reader:
+ * Recorder:
+ * Inspector:
+ * 
+ */
+
+package datamanagement;
+import java.util.*;
+import java.io.*;
+//making singleton class of AppProperties
+public class AppProperties {
+private static AppProperties self = null;
+private Properties properties;
+
+public static AppProperties getInstance() {
+	//if AppProperties null create new AppProperties Object
+	if (self == null ) { 
+		self = new AppProperties(); 
+		} return self;
+	}
+	//make AppProperties constructor private
+	private AppProperties() {
+		properties = new Properties(); //Create  Properties object
+		try {
+			//load properties file using FileInputStream
+			properties.load(new FileInputStream("Properties.prop"));
+			}
+		catch (IOException e){
+		//if IOException happened throw new RuntimeExeception
+		throw new RuntimeException("Could not read property file");
+			}
+		}
+   //create public method to return properties
+   public Properties getProperties() {return properties;
+   }
+}
